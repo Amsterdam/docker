7909da4b411904a60c6616764a3fc0aa366685bf
s.yaldiz <s.yaldiz@amsterdam.nl>
2019-01-04 11:39:16 +0100

    build git from sources for specific ubuntu releases

---
+++ git_openssl/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -u   # crash on missing env variables
+set -e   # stop on any error
+set -x
+
+source /etc/lsb-release
+echo "DISTRIB_RELEASE is:" $DISTRIB_RELEASE
+echo "DISTRIB_CODENAME is:" $DISTRIB_CODENAME
+
+docker-compose build --build-arg MAJOR_MINOR=$DISTRIB_RELEASE --build-arg CODE_NAME=$DISTRIB_CODENAME --no-cache
+docker-compose up --force-recreate
+docker cp "$(docker-compose ps -q)":/tmp/source-git/deb/ .
+dpkg -i deb/git-man*ubuntu$DISTRIB_RELEASE*.deb
+dpkg -i deb/git_*ubuntu$DISTRIB_RELEASE.1_amd64.deb
+docker-compose down --rmi all
